5042968b9129e67a8b2eceeb5eb9c39cdf745ebe
Nisarat Saelim <nisaratnsaelim@gmail.com>
2018-12-15 14:48:56 +0700

    add data

--- databsae.js
+++ databsae.js
@@ -6,3 +6,25 @@ var db = pgp('postgres://rqaybedddsjltk:522008ee90bcf532f956a16ebb8e86c1fad79742
 module.exports = {
     getAllProducts,
 };
+
+
+// Add queries here เพื่อในserverมาดึงข้อมูลนำไปใช้
+function getAllProducts(req, res) {
+    db.any('select * from products')
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved ALL products'
+                });
+        })
+        .catch(function (error) {
+            // console.log('ERROR:', error)
+            res.status(500)
+                .json({
+                    status: 'success',
+                    message: 'Failed to retrieved'
+                })
+        })
+}
