37b75f99c3e64edc0868eb0d6cb11602b39a86d1
Nisarat Saelim <nisaratnsaelim@gmail.com>
2018-12-15 15:06:11 +0700

    add package json

--- databsae.js
+++ databsae.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')();
-var db = pgp('postgres://rqaybedddsjltk:522008ee90bcf532f956a16ebb8e86c1fad79742480bee59d6f8af4459d35459@ec2-23-21-65-173.compute-1.amazonaws.com:5432/d4plclm0rplh3m?ssl=true');
+var db = pgp('?ssl=true');
 
 
 //functionไหนบ้างที่อยากจะให้serverเอาไปใช้บ้าง
